735046ee7bd258ba1282a4b23a3e6d1d50c8820a
Breno Pessoa <brenopessoamelo@gmail.com>
2018-02-08 01:18:47 -0300

    started key-value driver implemetatiom
    Signed-off-by: Breno Pessoa <brenopessoamelo@gmail.com>
    Signed-off-by: Breno Pessoa <brenopessoamelo@gmail.com>

--- dynamodb-driver/src/main/java/org/dynamodb/driver/key/DynamoDBBucketManager.java
+++ dynamodb-driver/src/main/java/org/dynamodb/driver/key/DynamoDBBucketManager.java
@@ -1,68 +1,152 @@
 package org.dynamodb.driver.key;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.json.bind.Jsonb;
 
 import org.jnosql.diana.api.Value;
 import org.jnosql.diana.api.key.BucketManager;
 import org.jnosql.diana.api.key.KeyValueEntity;
+import org.jnosql.diana.driver.ValueJSON;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.BatchGetItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.TableKeysAndAttributes;
+import com.amazonaws.services.dynamodbv2.document.TableWriteItems;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.TimeToLiveSpecification;
+import com.amazonaws.services.dynamodbv2.model.UpdateTimeToLiveRequest;
 
 public class DynamoDBBucketManager implements BucketManager{
 
+	private final AmazonDynamoDB client;
+	private final Table table;
+	private final DynamoDB dynamoDB;
+	private final Jsonb jsonB;
+	
+	public DynamoDBBucketManager(AmazonDynamoDB client, Table table, DynamoDB dynamoDB, Jsonb jsonB) {
+		this.client = client;
+		this.table = table;
+		this.dynamoDB = dynamoDB;
+		this.jsonB = jsonB;
+	}
+
 	@Override
 	public <K, V> void put(K key, V value) throws NullPointerException {
-		// TODO Auto-generated method stub
+		
+	    Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+	    AttributeValue attributeKey = new AttributeValue(key.toString());
+	    AttributeValue atrributeValue = new AttributeValue(jsonB.toJson(value.toString()));
+	    item.put("key",attributeKey);
+	    item.put("value", atrributeValue);
+	       
+	    PutItemRequest putItemRequest = new PutItemRequest(table.getTableName(),item);
+	    client.putItem(putItemRequest);
 		
 	}
 
 	@Override
 	public <K> void put(KeyValueEntity<K> entity) throws NullPointerException {
-		// TODO Auto-generated method stub
-		
+		put(entity.getKey(),entity.getValue());
 	}
 
 	@Override
 	public <K> void put(KeyValueEntity<K> entity, Duration ttl)
 			throws NullPointerException, UnsupportedOperationException {
-		// TODO Auto-generated method stub
 		
+        UpdateTimeToLiveRequest req = new UpdateTimeToLiveRequest();
+        req.setTableName(table.getTableName());
+
+        TimeToLiveSpecification ttlSpec = new TimeToLiveSpecification();
+        ttlSpec.setEnabled(true);
+        req.withTimeToLiveSpecification(ttlSpec);
+        
+        client.updateTimeToLive(req);
+        put(entity);
 	}
 
 	@Override
 	public <K> void put(Iterable<KeyValueEntity<K>> entities) throws NullPointerException {
-		// TODO Auto-generated method stub
 		
+		List<Item> items = StreamSupport.stream(entities.spliterator(),false)
+			.map(e -> new Item().withPrimaryKey("key",e.getKey().toString()))
+			.collect(Collectors.toList());
+		
+		TableWriteItems tableWriteItems = new TableWriteItems(table.getTableName()).withItemsToPut(items);
+		
+		dynamoDB.batchWriteItem(tableWriteItems);
 	}
 
 	@Override
 	public <K> void put(Iterable<KeyValueEntity<K>> entities, Duration ttl)
 			throws NullPointerException, UnsupportedOperationException {
-		// TODO Auto-generated method stub
+		
+		UpdateTimeToLiveRequest req = new UpdateTimeToLiveRequest();
+        req.setTableName(table.getTableName());
+
+        TimeToLiveSpecification ttlSpec = new TimeToLiveSpecification();
+        ttlSpec.setEnabled(true);
+        req.withTimeToLiveSpecification(ttlSpec);
+        
+        client.updateTimeToLive(req);
+        put(entities);
 		
 	}
 
 	@Override
 	public <K> Optional<Value> get(K key) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Objects.requireNonNull(key);
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey("key",key.toString());
+		Item item = table.getItem(spec);
+		Value of = ValueJSON.of(item.get("value"));
+		return Optional.ofNullable(of);
+		
 	}
 
 	@Override
 	public <K> Iterable<Value> get(Iterable<K> keys) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		TableKeysAndAttributes tableKeysAndAttributes = new TableKeysAndAttributes(table.getTableName());
+		StreamSupport.stream(keys.spliterator(),false)
+			.forEach(key -> tableKeysAndAttributes.addHashOnlyPrimaryKey("key",key.toString()));
+	
+		BatchGetItemOutcome batchGetItem = dynamoDB.batchGetItem(tableKeysAndAttributes);
+		
+		Map<String, List<Item>> itemsTable = batchGetItem.getTableItems();
+		
+		return StreamSupport.stream(itemsTable.keySet().spliterator(),false)
+			.flatMap(table -> itemsTable.get(table).stream())
+			.map(item -> ValueJSON.of(item.get("value")))
+			.collect(Collectors.toList());
 	}
 
 	@Override
 	public <K> void remove(K key) throws NullPointerException {
-		// TODO Auto-generated method stub
 		
+        DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
+        		.withPrimaryKey(new PrimaryKey("key",key.toString()));
+        
+        table.deleteItem(deleteItemSpec);
 	}
 
 	@Override
 	public <K> void remove(Iterable<K> keys) throws NullPointerException {
-		// TODO Auto-generated method stub
-		
+		StreamSupport.stream(keys.spliterator(),false).forEach(this::remove);
 	}
 
 	@Override
--- dynamodb-driver/src/main/java/org/dynamodb/driver/key/DynamoDBBucketManagerFactory.java
+++ dynamodb-driver/src/main/java/org/dynamodb/driver/key/DynamoDBBucketManagerFactory.java
@@ -1,19 +1,70 @@
 package org.dynamodb.driver.key;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
 import org.jnosql.diana.api.key.BucketManagerFactory;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+
 public class DynamoDBBucketManagerFactory implements BucketManagerFactory<DynamoDBBucketManager>{
+	
+	private final AmazonDynamoDB client = DynamoDBEmbedded.create().amazonDynamoDB();
+	private final DynamoDB dynamoDB = new DynamoDB(client);
+	private static final Jsonb JSON = JsonbBuilder.create();
 
 	@Override
 	public DynamoDBBucketManager getBucketManager(String bucketName)
 			throws UnsupportedOperationException, NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Objects.requireNonNull(bucketName,"teste");
+		
+		Table table = dynamoDB.getTable(bucketName);
+
+		if(table != null){
+		
+			String key = "key";
+	
+			List<KeySchemaElement> keySchema = Arrays.asList(new KeySchemaElement(key,KeyType.HASH));
+			List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+		    attributeDefinitions.add(new AttributeDefinition(key,ScalarAttributeType.S));
+		    
+		    ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L);
+		    
+		    CreateTableRequest createTableRequest = new CreateTableRequest()
+					.withTableName(bucketName)
+					.withAttributeDefinitions(attributeDefinitions)
+					.withProvisionedThroughput(provisionedthroughput)
+					.withKeySchema(keySchema);
+		    
+		    
+		   
+		    table = dynamoDB.createTable(createTableRequest);
+		    try {
+				table.waitForActive();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		return new DynamoDBBucketManager(client, table, dynamoDB,JSON);
 	}
 
 	@Override
